90e56fb38249cc5751d743edc1f24700b5cf46e8
cluelesprogrammer <sandesh.brl1@gmail.com>
2022-02-17 00:09:43 +0100

    random-changes

--- static/js/main.js
+++ static/js/main.js
@@ -11,30 +11,26 @@ navigator.mediaDevices.getUserMedia(constraints)
 .catch(function(err) { console.log(err.name + ": " + err.message); }); 
 
 function paddedFormat(x){
-  console.log(x)
+  return x;
 }
   
-function countdown(event) {
-  
-  let timersecs = parseInt(document.getElementById('timer-seconds').value);
-  let videooverlay = document.getElementById('countdown-text');
-  var mins = 0;
-  var secs = 0;
-
-  let countInterval = setInterval (function() {
-    mins = parseInt(timersecs / 60);
-    secs = parseInt(timersecs % 60); 
-
-    videooverlay.textContent = `${paddedFormat(mins)}:${paddedFormat(secs)}`;
-    timersecs = timersecs - 1;
-    if (timersecs < 0) { clearInterval(countInterval) };
-  }, 1000);
-  event.preventDefault();
-}
   
 var timerbutton = document.getElementById("timer-button");
 const uploadbutton = document.getElementById("upload-video");
+var form = document.getElementById('timer-form')
 
-var timerform = document.getElementById("timer-form");
-timerform.addEventListener("submit", countdown(event), false);
-
+form.addEventListener("submit", function(e) {
+    let timersecs = parseInt(document.getElementById('timer-seconds').value);
+    let videooverlay = document.getElementById('countdown-text');
+    var mins = 0;
+    var secs = 0;
+  
+    let countInterval = setInterval (function() {
+      mins = parseInt(timersecs / 60);
+      secs = parseInt(timersecs % 60); 
+  
+      videooverlay.textContent = `${paddedFormat(mins)}:${paddedFormat(secs)}`;
+      timersecs = timersecs - 1;
+      if (timersecs < 0) { clearInterval(countInterval) };
+  }, 1000);
+})
