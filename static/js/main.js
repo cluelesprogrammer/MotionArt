cb2d58792de746168e19b21b8a20f10cc214f274
cluelesprogrammer <sandesh.brl1@gmail.com>
2022-02-17 23:36:18 +0100

    older-broswer-compatability

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,48 @@
 const constraints = {video: true};
 
-navigator.mediaDevices.getUserMedia(constraints)
-.then(function(mediaStream) {
+if (navigator.mediaDevices === undefined) {
+  navigator.mediaDevices = {};
+}
+
+// Some browsers partially implement mediaDevices. We can't just assign an object
+// with getUserMedia as it would overwrite existing properties.
+// Here, we will just add the getUserMedia property if it's missing.
+if (navigator.mediaDevices.getUserMedia === undefined) {
+  navigator.mediaDevices.getUserMedia = function(constraints) {
+
+    // First get ahold of the legacy getUserMedia, if present
+    var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+    // Some browsers just don't implement it - return a rejected promise with an error
+    // to keep a consistent interface
+    if (!getUserMedia) {
+      return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+    }
+
+    // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+    return new Promise(function(resolve, reject) {
+      getUserMedia.call(navigator, constraints, resolve, reject);
+    });
+  }
+}
+
+navigator.mediaDevices.getUserMedia({ video: true })
+.then(function(stream) {
   var video = document.querySelector('video');
-  video.srcObject = mediaStream;
+  // Older browsers may not have srcObject
+  if ("srcObject" in video) {
+    video.srcObject = stream;
+  } else {
+    // Avoid using this in new browsers, as it is going away.
+    video.src = window.URL.createObjectURL(stream);
+  }
   video.onloadedmetadata = function(e) {
     video.play();
   };
 })
-.catch(function(err) { console.log(err.name + ": " + err.message); }); 
+.catch(function(err) {
+  console.log(err.name + ": " + err.message);
+});
 
 function paddedFormat(x){
   return x;
