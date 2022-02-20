74bb83796e65f703b64294b2d72f853cfd0bfa96
cluelesprogrammer <sandesh.brl1@gmail.com>
2022-02-20 17:19:40 +0100

    removed-alerts

--- static/js/main.js
+++ static/js/main.js
@@ -34,11 +34,10 @@ if (navigator.mediaDevices.getUserMedia === undefined) {
 navigator.mediaDevices.getUserMedia(constraints)
 .then(function(stream) {
   var video = document.querySelector('video');
-  alert(video);
   // Older browsers may not have srcObject
   if ("srcObject" in video) {
     video.srcObject = stream;
-    alert(stream);
+    
   } else {
     // Avoid using this in new browsers, as it is going away.
     video.src = window.URL.createObjectURL(stream);
